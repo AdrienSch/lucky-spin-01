66111fdb20509245a77ccf11ed6a67b3f035a381
aschi <aschildwachter@live.com>
2025-01-09 21:44:15 -0800

    committing final changes.

--- LuckySpin/Controllers/SpinnerController.cs
+++ LuckySpin/Controllers/SpinnerController.cs
@@ -11,22 +11,24 @@ namespace LuckySpin.Controllers
 
         public IActionResult Index(int luck) 
         {
-            //TODO: add your string builder and HTML
-        var rand = new Random();
+            //DONE: add your string builder and HTML
+            var rand = new Random();
             int[] spin = new int[] { rand.Next(0, 10), rand.Next(0, 10), rand.Next(0, 10) };
 
             System.Text.StringBuilder htmlToShow =  
-                new System.Text.StringBuilder("<body><h1>Lucky Spin</h1><button onclick='history.go(0)'>Spin</button>");
+                new System.Text.StringBuilder("<body><h1>Lucky Spin ");
+                    htmlToShow.Append(luck.ToString());
+                    htmlToShow.Append("</h1><button onclick='history.go(0)'>Spin</button>");
                     htmlToShow.Append("<div>" + spin[0] + "</div>");
                     htmlToShow.Append("<div>" + spin[1] + "</div>");
                     htmlToShow.Append("<div>" + spin[2] + "</div>");
-                if(spin[0] == 7 || spin[1] == 7 || spin[2] == 7) {
+                if(spin[0] == luck || spin[1] == luck || spin[2] == luck) {
             
             htmlToShow.Append("<img src='http://studentfolders.cascadia.edu/itweb285/LuckySpinCoins.jpg'/></body>");
           };
 
-            //TODO: Modify this to use the string builder's response string as the Content property's value
-            return new ContentResult { Content = "<h1>We're Ready to Spin with Controllers</h1>", ContentType="text/html"};
+            //DONE: Modify this to use the string builder's response string as the Content property's value
+            return new ContentResult { Content = htmlToShow.ToString(), ContentType="text/html"};
         }
     }
 }
